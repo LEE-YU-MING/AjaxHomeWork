904ad3ad362d98aed588c779dcf985a9d48ef521
YU-MING-LEE <karta1022686@gmail.com>
2024-01-23 09:23:27 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/APIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class APIController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+
+        public APIController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities=_dbContext.Addresses.Select(x => x.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
